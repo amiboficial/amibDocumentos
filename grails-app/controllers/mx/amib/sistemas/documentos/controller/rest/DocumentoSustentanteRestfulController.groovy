94ef74475512230abb4be764338f15486f1d8dcd
gabriel-medina <gabriel.m.g.245245@gmail.com>
2015-09-23 21:58:55 -0500

    DEV:
    desc: se añaden algunos update by uuid

--- grails-app/controllers/mx/amib/sistemas/documentos/controller/rest/DocumentoSustentanteRestfulController.groovy
+++ grails-app/controllers/mx/amib/sistemas/documentos/controller/rest/DocumentoSustentanteRestfulController.groovy
@@ -1,8 +1,10 @@
 package mx.amib.sistemas.documentos.controller.rest
 
 import static org.springframework.http.HttpStatus.*
+import grails.converters.JSON
 import grails.rest.RestfulController
 import grails.transaction.Transactional
+import mx.amib.sistemas.documentos.model.DocumentoRevocacion;
 import mx.amib.sistemas.documentos.model.DocumentoSustentante
 
 @Transactional(readOnly = false)
@@ -14,4 +16,29 @@ class DocumentoSustentanteRestfulController extends RestfulController {
 		super(DocumentoSustentante)
 	}
 	
+	def updateByUuid(DocumentoSustentante documentoSustentante){
+		HashMap<String,String> responseMap = new HashMap<String,String>()
+		DocumentoSustentante documentoSustentanteToUpdate = DocumentoSustentante.findByUuid(documentoSustentante.uuid)
+		
+		if(documentoSustentanteToUpdate != null){
+			documentoSustentanteToUpdate.fechaModificacion = new Date()
+			documentoSustentanteToUpdate.numeroMatricula = documentoSustentante.numeroMatricula
+			documentoSustentanteToUpdate.tipoDocumentoSustentante = documentoSustentante.tipoDocumentoSustentante
+			documentoSustentanteToUpdate.nombreCompleto = documentoSustentante.nombreCompleto
+			
+			try{
+				documentoSustentanteToUpdate.save(flush:true)
+				responseMap.put("status","OK")
+			}
+			catch(Exception e){
+				responseMap.put("status","ERROR")
+				responseMap.put("details",e.getMessage())
+			}
+		}
+		else{
+			responseMap.put("status","NOT_FOUND")
+		}
+		
+		render responseMap as JSON
+	}
 }
--- grails-app/controllers/mx/amib/sistemas/documentos/controller/rest/FotoSustenanteRestfulController.groovy
+++ grails-app/controllers/mx/amib/sistemas/documentos/controller/rest/FotoSustenanteRestfulController.groovy
@@ -1,8 +1,10 @@
 package mx.amib.sistemas.documentos.controller.rest
 
 import static org.springframework.http.HttpStatus.*
+import grails.converters.JSON
 import grails.rest.RestfulController
 import grails.transaction.Transactional
+import mx.amib.sistemas.documentos.model.FotoSustenante;
 import mx.amib.sistemas.documentos.model.FotoSustenante
 
 @Transactional(readOnly = false)
@@ -14,4 +16,28 @@ class FotoSustenanteRestfulController extends RestfulController {
 		super(FotoSustenante)
 	}
 	
+	def updateByUuid(FotoSustenante fotoSustenante){
+		HashMap<String,String> responseMap = new HashMap<String,String>()
+		FotoSustenante fotoSustenanteToUpdate = FotoSustenante.findByUuid(fotoSustenante.uuid)
+		
+		if(fotoSustenanteToUpdate != null){
+			fotoSustenanteToUpdate.fechaModificacion = new Date()
+			fotoSustenanteToUpdate.numeroMatricula = fotoSustenante.numeroMatricula
+			fotoSustenanteToUpdate.nombreCompleto = fotoSustenante.nombreCompleto
+			
+			try{
+				fotoSustenanteToUpdate.save(flush:true)
+				responseMap.put("status","OK")
+			}
+			catch(Exception e){
+				responseMap.put("status","ERROR")
+				responseMap.put("details",e.getMessage())
+			}
+		}
+		else{
+			responseMap.put("status","NOT_FOUND")
+		}
+		
+		render responseMap as JSON
+	}
 }
